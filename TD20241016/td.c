b9407feaa2804a071c5d7a1b42cd1e2eb71ea95a
MarquesChavesJoao <joao.marqueschaves@heig-vd.ch>
2024-10-16 08:00:21 +0000

    maj

--- TD20241016/td.c
+++ TD20241016/td.c
@@ -4,15 +4,34 @@
 
 int main(int argc, const char *argv[])
 {
-	int n = 1000;
+	int n = 42;
 	unsigned char count = 0;
+	float alpha = 60;
+	float result;
+	count = (int)log10(n) + 1;
+	printf("Nombre de digit (base 10): %d\n", count);
 
-    if (n == 0) {
-        count = 1;
-    } else {
-        count = log10(n) + 1;
-    }
+    count = (int)log2(n) + 1;
+    printf("Nombre de digit (base 2 ): %d\n", count);
 
-    printf("Nombre de chiffres: %d\n", count);
+	count = (int)(log10(n)/log10(16))+1;
+    printf("Nombre de digit (base 16): %d\n", count);
+
+ 	alpha = alpha * M_PI / 180.0;
+
+	result = sin(alpha);
+    printf("sin alpha : %.6f\n", result);
+
+	result = cos(alpha);
+    printf("cos alpha : %.6f\n", result);
+
+	result = tan(alpha);
+    printf("tan alpha : %.6f\n", result);
+
+	result = sin(alpha)/cos(alpha);
+    printf("tan alpha : %.6f\n", result);
+
+	result = tan(alpha)-(sin(alpha)/cos(alpha));
+    printf("tan alpha : %.10E\n", result);
 	return 0;
 }
